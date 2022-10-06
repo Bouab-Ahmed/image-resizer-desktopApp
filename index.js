c987251d2e1e05e3d6f08f33c39c66053df9630f
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2022-10-06 21:39:45 +0100

    add custome menu Roles

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 const isMac = process.platform === 'darwin';
 
+// create the main window
 const createMainWindow = () => {
   const window = new BrowserWindow({
     title: 'Image Resizer',
@@ -13,8 +14,43 @@ const createMainWindow = () => {
   window.loadFile(path.join(__dirname, './renderer/index.html'));
 };
 
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'about',
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'help',
+          submenu: [
+            {
+              label: 'About',
+            },
+          ],
+        },
+      ]
+    : []),
+];
+
+// app is ready
 app.whenReady().then(() => {
   createMainWindow();
+
+  //implement the menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
